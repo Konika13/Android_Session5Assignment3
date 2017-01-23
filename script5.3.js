7cf4d8afe0e94c893884e6ddffa513f56ac3eef4
Konika13 <konika.agarwal1994@gmail.com>
2017-01-24 02:14:07 +0530

    Add files via upload

---
+++ script5.3.js
@@ -0,0 +1,8 @@
+var obj = {"Employee":[{'name':'Konika Agarwal','age':22,'Salary':'1.5 lac','address':[{'city':'Rampur','state':'Uttar Pradesh','pincode':'244921'}]}]};
+
+console.log(obj.Employee[0].name)
+console.log(obj.Employee[0].age)
+console.log(obj.Employee[0].Salary)
+console.log(obj.Employee[0].address[0].city)
+console.log(obj.Employee[0].address[0].state)
+console.log(obj.Employee[0].address[0].pincode)
